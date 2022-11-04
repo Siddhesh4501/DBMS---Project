afe7657c5534e149850f64f76273fab7631831d3
ShyamR12 <shyamrandar12@gmail.com>
2022-11-04 21:52:42 +0530

    added sql queries
    backend connected. sample sql query for filtering questions based on various param added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 
 app.use(cors({
@@ -11,23 +11,87 @@ const db = mysql.createPool({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "mysqlpassword##1234",
-  database: "employeeSystem",
+  password: "coep@123",
+  database: "internship",
 });
 
-app.use(express.json());
+// app.use(express.json());
+// const first_name = "Siddhesh"
+// let sql = `SELECT *,"shyam" FROM Student where first_name = "${first_name}";`
 
-// db.connect(function(err) {
+// db.execute(sql, function (err, result) {
+//   if (err) throw err
+//   console.log(result)
+//   result.forEach(res => {
+//     console.log(res.title)
+//   })
+// })
+
+
+
+
+// filtering out questions based on company_name, tags(question,company_name), mis
+let company_lst = ["BNY Mellon", "Deutsche bank"]
+let company_str = '"' + company_lst.join('","') + '"';
+let mis_lst = ["112003116", "112003112", "112003114"]
+let mis_str = '"' + mis_lst.join('","') + '"';
+
+let sql_dsa = `SELECT dsa_questions.question_description,student.first_name,company.company_name,"DSA" as Tag FROM dsa_questions
+               JOIN company ON company.company_id = dsa_questions.company_id 
+               JOIN student ON student.mis = dsa_questions.mis 
+               WHERE company.company_name in (${company_str}) 
+               AND 
+               student.mis in (${mis_str});`
+
+let sql_core = `SELECT core_questions.question_description,student.first_name,company.company_name,"CORE" as Tag FROM core_questions
+               JOIN company ON company.company_id = core_questions.company_id 
+               JOIN student ON student.mis = core_questions.mis 
+               WHERE company.company_name in (${company_str}) 
+               AND 
+               student.mis in (${mis_str});`
+
+let sql_hr = `SELECT hr_questions.question_description,student.first_name,company.company_name,"HR" as Tag FROM hr_questions
+               JOIN company ON company.company_id = hr_questions.company_id 
+               JOIN student ON student.mis = hr_questions.mis 
+               WHERE company.company_name in (${company_str}) 
+               AND 
+               student.mis in (${mis_str});`
+
+db.execute(sql_dsa, function (err, result) {
+  if (err) throw err
+  console.log(result)
+  result.forEach(res => {
+    console.log(res.title)
+  })
+})
+
+db.execute(sql_hr, function (err, result) {
+  if (err) throw err
+  console.log(result)
+  result.forEach(res => {
+    console.log(res.title)
+  })
+})
+
+db.execute(sql_core, function (err, result) {
+  if (err) throw err
+  console.log(result)
+  result.forEach(res => {
+    console.log(res.title)
+  })
+})
+
+// db.connect(function (err) {
 //   if (err) {
 //     console.error('⚠️  Error Connecting: ' + err.stack);
 //     return;
 //   }
- 
+
 //   console.log('✅  Connected as ID: ' + connection.threadId);
 // });
 
-// app.post("/create", (req, res) => {
-//   const name = req.body.name;
+// app.post("/questions", (req, res) => {
+//   const question = req.body.name;
 //   const age = req.body.age;
 //   const country = req.body.country;
 //   const position = req.body.position;

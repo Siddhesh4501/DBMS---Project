464a792eada9e2d7d558837a10b8df47b8fbbb86
Siddhesh4501 <pawarsd20.comp@coep.ac.in>
2022-11-05 09:55:16 +0530

    added in stat

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -5,21 +5,85 @@ import Main from '../layouts/Main';
 
 import Personal from '../components/Stats/Personal';
 import Site from '../components/Stats/Site';
-
+import "./stats.css"
 const Stats = () => (
   <Main
     title="Stats"
     description="Some statistics about Michael D'Angelo and mldangelo.com"
   >
-    <article className="post" id="stats">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading"><Link to="/stats">Stats</Link></h2>
-        </div>
-      </header>
-      <Personal />
-      <Site />
-    </article>
+
+    
+<div className="content">
+  <div className="card">
+    <div className="firstinfo">
+      <div className="profileinfo">
+        <h1>Siddhesh Pawar</h1>
+        <h3>Barclays</h3>
+        <p className="bio"> Que. What are the different types of DSA questions asked in the interview.</p>
+        <button type="button" class="btn btn-success btn-sm">See all ans</button>
+        <button type="button" class="ml-5 btn btn-primary">Answer Que</button>
+      </div>
+    </div>
+  </div>
+  <div className="badgescard"> <span className="devicons devicons-django"></span><span className="devicons devicons-python"> </span><span className="devicons devicons-codepen"></span><span className="devicons devicons-javascript_badge"></span><span className="devicons devicons-gulp"></span><span className="devicons devicons-angular"></span><span className="devicons devicons-sass"></span></div>
+</div>
+
+<div className="content">
+  <div className="card">
+    <div className="firstinfo">
+      <div className="profileinfo">
+        <h1>Francesco Moustache</h1>
+        <h3>Python Ninja</h3>
+        <p className="bio">Lived all my life on the top of mount Fuji, learning the way to be a Ninja Dev.</p>
+        <button type="button" class="btn btn-success btn-sm">See all ans</button>
+        <button type="button" class="ml-5 btn btn-primary">Answer Que</button>
+      </div>
+    </div>
+  </div>
+  <div className="badgescard"> <span className="devicons devicons-django"></span><span className="devicons devicons-python"> </span><span className="devicons devicons-codepen"></span><span className="devicons devicons-javascript_badge"></span><span className="devicons devicons-gulp"></span><span className="devicons devicons-angular"></span><span className="devicons devicons-sass"></span></div>
+</div>
+<div className="content">
+  <div className="card">
+    <div className="firstinfo">
+      <div className="profileinfo">
+        <h1>Francesco Moustache</h1>
+        <h3>Python Ninja</h3>
+        <p className="bio">Lived all my life on the top of mount Fuji, learning the way to be a Ninja Dev.</p>
+        <button type="button" class="btn btn-success btn-sm">See all ans</button>
+        <button type="button" class="ml-5 btn btn-primary">Answer Que</button>
+      </div>
+    </div>
+  </div>
+  <div className="badgescard"> <span className="devicons devicons-django"></span><span className="devicons devicons-python"> </span><span className="devicons devicons-codepen"></span><span className="devicons devicons-javascript_badge"></span><span className="devicons devicons-gulp"></span><span className="devicons devicons-angular"></span><span className="devicons devicons-sass"></span></div>
+</div>
+<div className="content">
+  <div className="card">
+    <div className="firstinfo">
+      <div className="profileinfo">
+        <h1>Francesco Moustache</h1>
+        <h3>Python Ninja</h3>
+        <p className="bio">Lived all my life on the top of mount Fuji, learning the way to be a Ninja Dev.</p>
+        <button type="button" class="btn btn-success btn-sm">See all ans</button>
+        <button type="button" class="ml-5 btn btn-primary">Answer Que</button>
+      </div>
+    </div>
+  </div>
+  <div className="badgescard"> <span className="devicons devicons-django"></span><span className="devicons devicons-python"> </span><span className="devicons devicons-codepen"></span><span className="devicons devicons-javascript_badge"></span><span className="devicons devicons-gulp"></span><span className="devicons devicons-angular"></span><span className="devicons devicons-sass"></span></div>
+</div>
+<div className="content">
+  <div className="card">
+    <div className="firstinfo">
+      <div className="profileinfo">
+        <h1>Francesco Moustache</h1>
+        <h3>Python Ninja</h3>
+        <p className="bio">Lived all my life on the top of mount Fuji, learning the way to be a Ninja Dev.</p>
+        <button type="button" class="btn btn-success btn-sm">See all ans</button>
+        <button type="button" class="ml-5 btn btn-primary">Answer Que</button>
+      </div>
+    </div>
+  </div>
+  <div className="badgescard"> <span className="devicons devicons-django"></span><span className="devicons devicons-python"> </span><span className="devicons devicons-codepen"></span><span className="devicons devicons-javascript_badge"></span><span className="devicons devicons-gulp"></span><span className="devicons devicons-angular"></span><span className="devicons devicons-sass"></span></div>
+</div>
   </Main>
 );
 

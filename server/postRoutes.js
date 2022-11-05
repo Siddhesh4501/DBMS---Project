8b87c3c24a55474b8a591da7d4a8b651f8a8a307
ShyamR12 <shyamrandar12@gmail.com>
2022-11-05 14:53:49 +0530

    added post question functionality
    filters by question_tag and then inserts the record in apt table

--- server/postRoutes.js
+++ server/postRoutes.js
@@ -5,4 +5,5 @@ const router = express.Router()
 router.route("/student").get(postControllers.getAllPostsStudent).post(postControllers.createNewPostStudent)
 router.route("/company").get(postControllers.getAllPostsCompany).post(postControllers.createNewPostCompany)
 router.route("/interview-experience").get(postControllers.getAllPostsInterviewExperience).post(postControllers.createNewPostInterviewExperience)
+router.route("/questions").get(postControllers.getAllPostsQuestions).post(postControllers.createNewPostQuestions)
 module.exports = router

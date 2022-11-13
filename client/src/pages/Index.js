bf25c90758e225a28ba01a577d16dff3b2a858cd
Pratik Patil <87142754+pt3002@users.noreply.github.com>
2022-11-13 12:12:59 +0530

    Filter Changes

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,33 +1,167 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import Select from "react-select";
 
-import Main from '../layouts/Main';
+import Main from "../layouts/Main";
 
-const Index = () => (
-  <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
-  >
-    <article className="post" id="index">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
+const Index = () => {
+  const [company, setCompany] = useState("");
+  const [verdict, setVerdict] = useState("");
+
+  const [companyFilter, setCompanyFilter] = React.useState("No");
+  const [verdictFilter, setVerdictFilter] = React.useState("No");
+
+  const handleFilterChange = (event) => {
+    let a = event.target.value;
+
+    if (a === "Company Filter") {
+      setCompanyFilter("Yes");
+      setVerdictFilter("No");
+    }
+    if (a === "Verdict Filter") {
+      setVerdictFilter("Yes");
+      setCompanyFilter("No");
+    }
+    if (a === "Both") {
+      setVerdictFilter("Yes");
+      setCompanyFilter("Yes");
+    }
+  };
+
+  let company_lst = [];
+
+  Axios.get("http://localhost:3001/posts//getCompanyFromExpe").then((res) => {
+    res.data.map((val) => {
+      let tmp = { label: val.company_name, value: val.company_name };
+      company_lst.push(tmp);
+    });
+  });
+  let data = [];
+  let verdict_list = [];
+  verdict_list.push({ label: "Selected", value: "Selected" });
+  verdict_list.push({ label: "Not Selected", value: "Not Selected" });
+
+  function onNext() {
+    data = [];
+    Axios.get("http://localhost:3001/posts/getExpeCompanyWise").then((res) => {
+      res.data.map((val) => {
+        if (companyFilter === "Yes" && verdictFilter === "No") {
+          if (val.company_name === company) {
+            data.push(val);
+          }
+        }
+        if (verdictFilter === "Yes" && companyFilter === "No") {
+          if (val.verdict === verdict) {
+            data.push(val);
+          }
+        }
+        if (verdictFilter === "Yes" && companyFilter === "Yes") {
+          if (val.verdict === verdict && val.company_name === company) {
+            data.push(val);
+          }
+        }
+      });
+    });
+
+    console.log(data);
+  }
+
+  return (
+    <Main
+      description={
+        "Michael D'Angelo's personal website. New York based Stanford ICME graduate, " +
+        "co-founder and CTO of Arthena, and YC Alumni."
+      }
+    >
+      <div>
+        <label htmlFor="company_name">
+          Do you want to filter by Company, Verdict or by both?
+        </label>
+        <input
+          type="radio"
+          id="Company Filter"
+          name="choose"
+          value="Company Filter"
+          onChange={handleFilterChange}
+        />
+        <label htmlFor="Company Filter">Company Filter</label>
+
+        <input
+          type="radio"
+          id="Verdict Filter"
+          name="choose"
+          value="Verdict Filter"
+          onChange={handleFilterChange}
+        />
+        <label htmlFor="Verdict Filter">Verdict Filter</label>
+
+        <input
+          type="radio"
+          id="Both"
+          name="choose"
+          value="Both"
+          onChange={handleFilterChange}
+        />
+        <label htmlFor="Both">Both Company and Verdict</label>
+      </div>
+
+      {companyFilter === "Yes" && (
+        <div className="form-group">
+          <label htmlFor="company_name">Filter By Company</label>
+          <Select
+            className="drop-down"
+            options={company_lst}
+            name="company"
+            onChange={(opt) => {
+              Select.value = opt.value;
+              setCompany(opt.value);
+            }}
+          />
         </div>
-      </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
-      </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
-    </article>
-  </Main>
-);
+      )}
+
+      {verdictFilter === "Yes" && (
+        <div className="form-group">
+          <label htmlFor="company_name">
+            Filter By Verdict (Selected/ Not Selected)
+          </label>
+          <Select
+            className="drop-down"
+            options={verdict_list}
+            name="verdict"
+            onChange={(opt) => {
+              Select.value = opt.value;
+              setVerdict(opt.value);
+            }}
+          />
+        </div>
+      )}
+
+<article className="post" id="index">
+              <header>
+                <div className="title">
+                  <h2 data-testid="heading">
+                    <Link to="/">About this site</Link>
+                  </h2>
+                </div>
+              </header>
+              <p>e0irji0r</p>
+            </article>
+
+            
+
+      <button
+        type="submit"
+        className="button is-block is-info is-fullwidth"
+        onClick={() => {
+          onNext();
+        }}
+      >
+        View Experience
+      </button>
+    </Main>
+  );
+};
 
 export default Index;
